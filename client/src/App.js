fa2f034f081a15f490d18f848730136ed9e7f70a
impishmoon <idkwhoiam15op@gmail.com>
2023-01-31 07:34:30 +0200

    added api request on frontend and added backend filter by user email

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,26 @@
 import ListHeader from "./components/ListHeader";
+import { useEffect } from 'react'
 // import ListItem from "./components/ListItem";
 
 const App = () => {
+
+  const getData = async () => {
+    const userEmail = 'okaybro@org.com'
+
+    try {
+      const res = await fetch(`http://localhost:8000/todos/${userEmail}`)
+      const json = await res.json()
+      console.log(json)
+    }
+    catch (err) {
+      console.error(err)
+    }
+  }
+
+  useEffect(() =>
+    getData(), []
+  )
+
   return (
     <div className="app">
       <ListHeader listName={'🏝 Holiday Todo List'} />
--- server/server.js
+++ server/server.js
@@ -1,17 +1,21 @@
 const PORT = process.env.PORT ?? 8000;
 const express = require('express');
+const cors = require('cors');
 const postgres = require('pg');
 const app = express();
-const pool = require('./db')
+const pool = require('./db');
+
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send("hello word");
 })
-
-app.get('/todos', async (req, res) => {
+app.get('/todos/:userEmail', async (req, res) => {
+    console.log(req)
+    const {userEmail} = req.params
     try {
-        const todos = await pool.query('SELECT * FROM todos')
-        res.json(todos.rows)
+        const todos = await pool.query('SELECT * FROM todos WHERE user_email = $1', [userEmail])
+        res.json(todos.rows)                                        
     } catch (err) {
         console.error(err)
     }
