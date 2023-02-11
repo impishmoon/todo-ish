4d55846a13fedf7ad2b3545a7a66c9a043cbba95
impishmoon <77268045+impishmoon@users.noreply.github.com>
2023-02-12 01:23:04 +0200

    Added Signout Functionality

--- client/src/components/ListHeader.js
+++ client/src/components/ListHeader.js
@@ -1,10 +1,14 @@
 import { useState } from "react";
 import Modal from "./Modal";
+import { useCookies } from "react-cookie";
 
 const ListHeader = ({ listName, getData }) => {
+    const [cookies, setCookie, removeCookie] = useCookies(null)
     const [showModal, setShowModal] = useState(false)
     const signOut = () => {
-        console.log("signedout")
+        removeCookie('Email');
+        removeCookie('AuthToken');
+        window.location.reload()
     }
     return (
         <div className="list-header">
