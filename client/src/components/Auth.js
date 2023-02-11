b8981baaf763034bca0e120bb43fced018dc0516
impishmoon <77268045+impishmoon@users.noreply.github.com>
2023-02-12 00:31:54 +0200

    fixed weird signup/login buttons color glitching

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useCookies } from 'react-cookie';
 const Auth = () => {
   const [cookies, setCookie, removeCookie] = useCookies(null)
-  const [loggedIn, setloggedIn] = useState(false)
+  const [IsloggedIn, setIsloggedIn] = useState(false)
   const [email, setemail] = useState(null)
   const [password, setpassword] = useState(null)
   const [confirmpassword, setconfirmpassword] = useState(null)
@@ -10,12 +10,12 @@ const Auth = () => {
 
   const viewLogin = (status) => {
     setError(null);
-    setloggedIn(status);
+    setIsloggedIn(status);
   }
 
   const handleSubmit = async (e, endpoint) => {
     e.preventdefault();
-    if (!loggedIn && password !== confirmpassword) {
+    if (!IsloggedIn && password !== confirmpassword) {
       setError('Passwords don\'t match')
     }
     const res = await fetch(`${process.env.REACT_APP_SERVER_URL}/${endpoint}`, {
@@ -36,21 +36,21 @@ const Auth = () => {
     <div className="auth-container">
       <div className="auth-container-box">
         <form className="auth-form">
-          <h2>{loggedIn ? 'login' : 'signup'}</h2>
+          <h2>{IsloggedIn ? 'login' : 'signup'}</h2>
           <input type="email" placeholder="Your email goes here" onChange={(e) => setemail(e.target.value)} />
           <input type="password" placeholder="Your password goes here" onChange={(e) => password(e.target.value)} />
-          {!loggedIn && <input type="password" placeholder="Repeat your password" onChange={(e) => setconfirmpassword(e.target.value)} />}
-          <input className='create' type='submit' value='SUBMIT' onClick={(e) => handleSubmit(e, loggedIn ? 'login' : 'signup')} />
+          {!IsloggedIn && <input type="password" placeholder="Repeat your password" onChange={(e) => setconfirmpassword(e.target.value)} />}
+          <input className='create' type='submit' value='SUBMIT' onClick={(e) => handleSubmit(e, IsloggedIn ? 'login' : 'signup')} />
           {error && <p>{error}</p>}
         </form>
         <div className="auth-options">
           <button
             onClick={() => viewLogin(false)}
-            style={{ backgroundColor: !loggedIn ? 'rgb(255,255,255)' : 'rgb(188,188,188)' }}
+            style={{ backgroundColor: IsloggedIn ? 'rgb(255,255,255)' : 'rgb(188,188,188)' }}
           >Sign Up</button>
           <button
             onClick={() => viewLogin(true)}
-            style={{ backgroundColor: !loggedIn ? 'rgb(255,255,255)' : 'rgb(188,188,188)' }}
+            style={{ backgroundColor: !IsloggedIn ? 'rgb(255,255,255)' : 'rgb(188,188,188)' }}
           >Login</button>
         </div>
       </div>
