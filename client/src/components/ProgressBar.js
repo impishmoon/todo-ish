27e8959a13fd21f1f5a1f616d5da6a5688ce4ba3
impishmoon <77268045+impishmoon@users.noreply.github.com>
2023-02-12 15:50:29 +0200

    made code a bit more readable

--- client/src/components/ProgressBar.js
+++ client/src/components/ProgressBar.js
@@ -1,27 +1,20 @@
 const ProgressBar = ({ progress }) => {
-  const colors = [
-    'rgb(255,175,163)',
-    'rgb(255,214,161)',
-    'rgb(130, 172, 206)',
-    'rgb(141,181,145)'
-  ]
-  const choosecorrectcolor = (progress) => {
-    let floored= Math.floor(progress)
-    if(floored<=25)
-    return colors[0]
-    if(floored<=50)
-    return colors[1]
-    if(floored<=75)
-    return colors[2]
-    if(floored>=80)
-    return colors[3]
-
+  const getProgressColor = (progress) => {
+    let floored = Math.floor(progress)
+    if (floored <= 25)
+      return 'rgb(255,175,163)'
+    if (floored <= 50)
+      return 'rgb(255,214,161)'
+    if (floored <= 75)
+      return 'rgb(130, 172, 206)'
+    if (floored >= 80)
+      return 'rgb(141,181,145)'
   }
 
   return (
     <div className="outer-bar">
       <div className="inner-bar"
-        style={{ width: `${progress}%`, backgroundColor: `${choosecorrectcolor(progress)}` }}>
+        style={{ width: `${progress}%`, backgroundColor: `${getProgressColor(progress)}` }}>
 
 
       </div>
