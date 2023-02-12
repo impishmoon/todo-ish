75fb98f17c811bee119a6d808d913119eb2c3c08
impishmoon <77268045+impishmoon@users.noreply.github.com>
2023-02-12 15:40:46 +0200

    added ProgressBar support and stylzied colors to be accurate by value

--- client/src/components/ProgressBar.js
+++ client/src/components/ProgressBar.js
@@ -1,9 +1,32 @@
-const ProgressBar = () =>{
-    return (
-      <div className="ProgressBar">
-      </div>
-    )
+const ProgressBar = ({ progress }) => {
+  const colors = [
+    'rgb(255,175,163)',
+    'rgb(255,214,161)',
+    'rgb(130, 172, 206)',
+    'rgb(141,181,145)'
+  ]
+  const choosecorrectcolor = (progress) => {
+    let floored= Math.floor(progress)
+    if(floored<=25)
+    return colors[0]
+    if(floored<=50)
+    return colors[1]
+    if(floored<=75)
+    return colors[2]
+    if(floored>=80)
+    return colors[3]
+
   }
-  
-  export default ProgressBar
-  
+
+  return (
+    <div className="outer-bar">
+      <div className="inner-bar"
+        style={{ width: `${progress}%`, backgroundColor: `${choosecorrectcolor(progress)}` }}>
+
+
+      </div>
+    </div>
+  )
+}
+
+export default ProgressBar
